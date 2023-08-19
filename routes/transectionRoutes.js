eae1ba5e7dab13841abac55898eb3f7cabd4b179
Sivabalan G <122631523+gsivabalan@users.noreply.github.com>
2023-08-19 08:14:29 +0530

    Update transectionRoutes.js

--- routes/transectionRoutes.js
+++ routes/transectionRoutes.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const { addTransection, getAllTransection, editTransection, deleteTransection } = require("../controllers/transectionCtrl");
 
-
-//router object
 const router = express.Router();
 
-//routers
 router.post('/add-transection', addTransection)
 router.post('/edit-transection', editTransection)
 router.post('/delete-transection', deleteTransection)
